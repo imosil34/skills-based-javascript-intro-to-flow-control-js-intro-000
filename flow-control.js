b4307d3e26e38e37c1f92ce3d62f822f38e7cad3
IMOH SILAS <imosil34@gmail.com>
2020-05-03 03:19:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,7 @@ function basicTeenager(age) {
 if (age === 13 && age <= 19){
 return "You are a teenager!";
 }
-
+}
 function teenager(age) {
 if (age === 13 && age <= 19){
   return "You are a teenager";
@@ -10,12 +10,24 @@ if (age === 13 && age <= 19){
 else{
   return "You are not a teenager";
 }
-function ageChecker(age) {
-
 }
-
+function ageChecker(age) {
+  if (age === 13 && age <= 19){
+  return "You are a teenager!";
+}else if (age <= 13){
+  return "You are a kid";
+}
+else {
+  return "You are a grownup";
+}
+}
 function ternaryTeenager(age) {
-
+  if (age === 13 && age <= 19){
+  return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
 }
 
 function switchAge(age) {
